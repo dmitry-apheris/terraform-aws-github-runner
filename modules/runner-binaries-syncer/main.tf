1fe65ac14d8cb5bb91ee464981d870b0d9929d3d
Dmitry Vorobiov <d.vorobiov@apheris.com>
2022-03-01 16:54:38 +0100

    Try to update S3 bucket config to version 4.12

--- modules/runner-binaries-syncer/main.tf
+++ modules/runner-binaries-syncer/main.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "aws_s3_bucket" "action_dist" {
   bucket        = var.distribution_bucket_name
-  acl           = "private"
+  # acl           = "private"
   force_destroy = true
   tags          = var.tags
 
